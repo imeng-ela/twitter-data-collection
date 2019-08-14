7914a3ca1b59fda5a7bfde659e3509cd6b93b849
Cloud User <centos@ip-172-31-57-171.ec2.internal>
2019-08-14 19:31:21 +0000

    First cut an sentiment analysis

---
+++ data-prep/sentiment.py
@@ -0,0 +1,77 @@
+import os
+import settings
+import boto3
+from botocore.exceptions import ClientError
+import tarfile
+import shutil
+import json
+
+## Bucket to use
+inBucketName = os.environ.get('S3_BUCKET_OUT')
+inDirName = os.environ.get('WORK_DIR_IN')
+outDirName = os.environ.get('WORK_DIR_OUT')
+translator = os.environ.get('TRANSLATOR')
+
+#Create working directories
+if not os.path.exists(outDirName):
+   os.mkdir(outDirName)
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(inBucketName)
+comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+
+#List objects within a given prefix
+for obj in bucket.objects.filter(Prefix='translated/twitterData'):
+   tgzFile = obj.key
+   print(tgzFile)
+
+   #Create working directories
+   if not os.path.exists(inDirName+'/translated'):
+      os.mkdir(inDirName)
+      os.mkdir(inDirName+'/translated')
+
+   # Get the object
+   fileName = inDirName+'/'+tgzFile
+   bucket.download_file(tgzFile, fileName)
+
+   # Untar the object
+   tar = tarfile.open(fileName)
+   tar.extractall(path=inDirName)
+   tar.close()
+
+   # Tranlate data
+   for filename in os.listdir(inDirName):
+      if filename.endswith(".json") and os.stat(os.path.join(inDirName, filename)).st_size != 0: 
+         pathFileName = os.path.join(inDirName, filename)
+         print(pathFileName)
+         with open(pathFileName,'r') as inFile:
+            try:
+               data = json.load(inFile)
+               # TODO: Hash tags, retweet status and others to translate
+               dataStr = data['text']
+               sentiment = json.dumps(comprehend.detect_sentiment(Text=dataStr, LanguageCode='en'), sort_keys=True, indent=4)
+	
+               pathFileName = os.path.join(outDirName, filename)
+               with open(pathFileName, 'w') as outFile:
+                  outFile.write(sentiment) 
+            except json.decoder.JSONDecodeError as e:
+               print(e)
+            inFile.close()
+      else:
+         continue
+
+   # Retar data
+   with tarfile.open(tgzFile[11:], "w:gz") as tar:
+      tar.add(outDirName, arcname=os.path.basename(outDirName))
+
+   # Copy to "translated" folder
+   s3.meta.client.upload_file(tgzFile[11:], inBucketName, 'sentiment/'+tgzFile[11:])
+
+   # Cleanup
+   try:
+      os.remove(tgzFile[11:])
+      shutil.rmtree(inDirName)
+      shutil.rmtree(outDirName)
+   except OSError as e:
+      print ("Error: %s - %s." % (e.filename, e.strerror))
+
